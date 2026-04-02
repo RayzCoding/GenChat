c558b97ccb4f4e14d985c09148db6f6eda67b392
RayzCoding <965178162@qq.com>
2026-04-02 22:50:01 +0800

    Feat add tavily tool

---
+++ src/main/java/com/genchat/config/WebSearchToolInitConfig.java
@@ -0,0 +1,58 @@
+package com.genchat.config;
+
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpRequest;
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class WebSearchToolInitConfig {
+
+    /**
+     * Tavily API Key
+     */
+    @Value("${tavily.api-key}")
+    private String tavilyApiKey;
+
+    /**
+     * Tavily MCP URL
+     */
+    @Value("${tavily.mcp-url}")
+    private String tavilyMcpUrl;
+
+    private ToolCallback[] webSearchToolCallbacks;
+
+    @PostConstruct
+    public void init() {
+        log.info("Initialize web search tool callback...");
+
+        // tavily 搜索引擎
+        String authorizationHeader = "Bearer " + tavilyApiKey;
+
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                .header("Authorization", authorizationHeader);
+
+        HttpClientStreamableHttpTransport tavTransport = HttpClientStreamableHttpTransport.builder(tavilyMcpUrl)
+                .requestBuilder(requestBuilder).build();
+        McpSyncClient tavilyMcp = McpClient.sync(tavTransport)
+                .requestTimeout(Duration.ofSeconds(300))
+                .build();
+        tavilyMcp.initialize();
+
+        List<McpSyncClient> mcpClients = List.of(tavilyMcp);
+        SyncMcpToolCallbackProvider provider = SyncMcpToolCallbackProvider.builder().mcpClients(mcpClients).build();
+
+        webSearchToolCallbacks = provider.getToolCallbacks();
+        log.info("Web search tool callback initialization complete, number of tools: {}", webSearchToolCallbacks.length);
+    }
+}
